67cfdc46651467e1b4c1d76379db660d84d16247
scaramallion <scaramallion@users.noreply.github.com>
2024-01-04 15:04:37 +1100

    Add HTJ2K files (#14)

--- ljdata/manager.py
+++ ljdata/manager.py
@@ -8,15 +8,39 @@ except ImportError:
     HAS_PYDICOM = False
 
 from ljdata.ds import (
-    JPEG2000_IDX, JPEG2000Lossless_IDX, JPEGBaseline_IDX,
-    JPEGExtended_IDX, JPEGLossless_IDX, JPEGLosslessSV1_IDX,
-    JPEGLS_IDX, JPEGLSLossless_IDX,  LittleEndianExplicit_IDX,
-    RLELossless_IDX, LittleEndianImplicit_IDX
+    JPEG2000_IDX,
+    JPEG2000Lossless_IDX,
+    JPEGBaseline_IDX,
+    JPEGExtended_IDX,
+    JPEGLossless_IDX,
+    JPEGLosslessSV1_IDX,
+    JPEGLS_IDX,
+    JPEGLSLossless_IDX,
+    LittleEndianExplicit_IDX,
+    LittleEndianImplicit_IDX,
+    RLELossless_IDX,
 )
 
 DATA_DIRECTORY = Path(__file__).resolve().parent
 DS_DIR = DATA_DIRECTORY / 'ds'
 JPG_DIR = DATA_DIRECTORY / 'jpg'
+
+
+UIDS = {
+    '1.2.840.10008.1.2':       'LittleEndianImplicit',
+    '1.2.840.10008.1.2.1':     'LittleEndianExplicit',
+    '1.2.840.10008.1.2.4.50':  'JPEGBaseline',
+    '1.2.840.10008.1.2.4.51':  'JPEGExtended',
+    '1.2.840.10008.1.2.4.57':  'JPEGLossless',
+    '1.2.840.10008.1.2.4.70':  'JPEGLosslessSV1',
+    '1.2.840.10008.1.2.4.80':  'JPEGLSLossless',
+    '1.2.840.10008.1.2.4.81':  'JPEGLS',
+    '1.2.840.10008.1.2.4.90':  'JPEG2000Lossless',
+    '1.2.840.10008.1.2.4.91':  'JPEG2000',
+    '1.2.840.10008.1.2.4.201': 'HTJ2KLossless',
+    '1.2.840.10008.1.2.4.203': 'HTJ2K',
+    '1.2.840.10008.1.2.5':     'RLELossless',
+}
 
 
 def get_datasets(uid, as_dataset=False):
@@ -34,20 +58,7 @@ def get_datasets(uid, as_dataset=False):
     -------
     Union[List[Path], List[Dataset]]
     """
-    uids = {
-        '1.2.840.10008.1.2':    'LittleEndianImplicit',
-        '1.2.840.10008.1.2.1':    'LittleEndianExplicit',
-        '1.2.840.10008.1.2.4.50': 'JPEGBaseline',
-        '1.2.840.10008.1.2.4.51': 'JPEGExtended',
-        '1.2.840.10008.1.2.4.57': 'JPEGLossless',
-        '1.2.840.10008.1.2.4.70': 'JPEGLosslessSV1',
-        '1.2.840.10008.1.2.4.80': 'JPEGLSLossless',
-        '1.2.840.10008.1.2.4.81': 'JPEGLS',
-        '1.2.840.10008.1.2.4.90': 'JPEG2000Lossless',
-        '1.2.840.10008.1.2.4.91': 'JPEG2000',
-        '1.2.840.10008.1.2.5':    'RLELossless',
-    }
-    subdir = uids[uid]
+    subdir = UIDS[uid]
     fnames = get_indices('ds')[subdir].keys()
     fpaths = [DS_DIR / subdir / fname for fname in fnames]
     if as_dataset:
@@ -60,20 +71,7 @@ def get_datasets(uid, as_dataset=False):
 
 
 def get_indexed_datasets(uid):
-    uids = {
-        '1.2.840.10008.1.2' : 'LittleEndianImplicit',
-        '1.2.840.10008.1.2.1' : 'LittleEndianExplicit',
-        '1.2.840.10008.1.2.4.50' : 'JPEGBaseline',
-        '1.2.840.10008.1.2.4.51' : 'JPEGExtended',
-        '1.2.840.10008.1.2.4.57' : 'JPEGLossless',
-        '1.2.840.10008.1.2.4.70' : 'JPEGLosslessSV1',
-        '1.2.840.10008.1.2.4.80' : 'JPEGLSLossless',
-        '1.2.840.10008.1.2.4.81' : 'JPEGLS',
-        '1.2.840.10008.1.2.4.90' : 'JPEG2000Lossless',
-        '1.2.840.10008.1.2.4.91' : 'JPEG2000',
-        '1.2.840.10008.1.2.5' : 'RLELossless',
-    }
-    subdir = uids[uid]
+    subdir = UIDS[uid]
     index = get_indices('ds')[subdir]
     fnames = index.keys()
     for fname in fnames:
